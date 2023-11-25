adf5c7c75bcf9a4ffb7e4ab1490e5c6d51d2b2b1
Jane Nicole Apostol <89412671+jnapostol@users.noreply.github.com>
2023-11-25 00:00:24 -0500

    basic idea

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    // menu options
+
+    // input parsing
+
     return 0;
 }
+
+// two diff classes to store data/process it?
+
+// storing via map (hash?) & max heap
+
+
