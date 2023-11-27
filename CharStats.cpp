f22c4e6ebecb675e129851e66789860d9f4eae5e
Veronica Yap <72471933+vnica-blip@users.noreply.github.com>
2023-11-27 14:55:24 -0500

    Added data files & CharStats class
    I used some of my old prog 2 file IO code-- this will be revised later.

---
+++ CharStats.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vnica on 11/27/2023.
+//
+
+#include "CharStats.h"
--- main.cpp
+++ main.cpp
@@ -1,9 +1,66 @@
 #include <iostream>
+#include <iomanip>
+#include <iostream>
+#include <fstream>		// Access to of-, if-, fstream
+#include <string>
+#include "CharStats.h"
+using namespace std;
+
+void GetDataFromCSVFile(const char* filepath)
+{
+    ifstream inFile(filepath);
+    inFile.open(filepath);
+
+    if (inFile.is_open())
+    {
+        // Read the heading data from the file
+        string lineFromFile;
+        getline(inFile, lineFromFile);
+
+        // Get all entries from the file, one line at a time
+        while (getline(inFile, lineFromFile))
+        {
+            // Create a stream from the line of data from the file
+            istringstream stream(lineFromFile);
+
+            string setName = "";
+            string setTheme = "";
+            int setNumber = 0;
+            int numberMinifigures = 0;
+            int pieceCount = 0;
+            float setPrice = 0;
+
+            string temp_setNumber, temp_numberMinifigures, temp_pieceCount;		// Tokens to be converted from string into original datatypes
+            string temp_setPrice;
+
+            CharStats s;		// Create object s, which is an individual legoset
+
+            getline(stream, temp_setNumber, ',');		// Separate the single line of data into its constituent parts
+            s.setNumber = stoi(temp_setNumber);			// Set each of these values as an attribute of the legoset
+
+            legosets.push_back(s);		// Add that legoset to the vector of legosets
+
+        }
+    }
+    else
+    {
+        cout << filepath << " was not opened!" << endl;
+    }
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    const char* filepath1 = "data/Game_Scoreboard.xlsx";
+    const char* filepath2 = "data/Games.xlsx";
+
+    
 
     // menu options
+    cout << "Welcome to our Professional Valorant Match Agent Statistics program!" << endl;
+    cout << "Do you want to get better at Valorant? See what the pros are doing!" << endl;
+    cout << "Pick a data structure to load the stats into." << endl;
+    cout << "1) Map" << endl << "2) Max Heap" << endl;
 
     // input parsing
 
